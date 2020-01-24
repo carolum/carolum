85d1e977a4cca895af70c568fcd8d3f6bd0ede27
JoshDaBosh <jwanggt@gmail.com>
2020-01-23 21:07:08 -0500

    signups done

---
+++ web/public/signup/js/index.js
@@ -0,0 +1,51 @@
+var signup = new Vue({
+	el: "#signup",
+	data: {
+		e: "",
+		p:"",
+		p2:"",
+		nomatch: false,
+		b:"",
+	},
+	methods:{
+		validate: function() {
+			this.nomatch = this.p2 !== "" ? this.p !== this.p2 : false;
+			
+		},
+		signup: function(){
+			if (!this.nomatch){
+				firebase.auth().createUserWithEmailAndPassword(this.e, this.p)
+					.catch(
+					function(error){
+						var errorCode = error.code,
+							error = "";
+						
+						switch(errorCode){
+							case "auth/weak-password":
+								error = "Password is too weak!";
+								break;
+								
+							case "auth/invalid-email":
+								error = "Invalid email!";
+								break;
+								
+							case "auth/email-already-in-use":
+								error = "Email already in use!";
+								break;
+								
+						}
+
+						$("#error").text(error);
+
+						console.log(error)
+					})
+			}
+		}
+	}
+});
+
+firebase.auth().onAuthStateChanged(function(u){
+	if(u){
+		window.location.href = '/';
+	}
+})
