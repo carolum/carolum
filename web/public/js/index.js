b4c34bae3362267df135f771625848d9bd889616
JoshDaBosh <jwanggt@gmail.com>
2020-02-08 22:07:31 -0500

    surprisingly, the hardest part about this project is GETTING THE TEXTAREA TO TAKE UP THE RIGHT AMOUNT OF SPACE IT STILL DOESN'T WANT TO

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -11,8 +11,31 @@ var nav = new Vue({
 });
 
 window.onhashchange = ()=> {
-	nav.tabs = location.hash.slice(1)
+	nav.tabs = ["new", "dash", "settings"].indexOf(location.hash.slice(1)) === -1 ? "new" : location.hash.slice(1);
+	location.hash = nav.tabs;
 };
+
+
+var newEntry = new Vue({
+	el: "#new",
+	computed: {
+		display: function() { return nav.tabs === 'new'; }
+	}
+});
+
+var dashboard = new Vue({
+	el: "#dash",
+	computed: {
+		display: function() { return nav.tabs === 'dash'; }
+	}
+});
+
+var settings = new Vue({
+	el: "#settings",
+	computed: {
+		display: function() { return nav.tabs === 'settings'; }
+	}
+});
 
 
 
